72c93ff702ae0153f4d0a2a785c7a22e3aaf84c0
carlos <carlos@Martinez>
2017-05-20 00:05:05 -0500

    EL PUTO COMMIT

--- hospital/hospital-negocio/src/main/java/beans/EJBUsuario.java
+++ hospital/hospital-negocio/src/main/java/beans/EJBUsuario.java
@@ -3,6 +3,11 @@ import java.io.Serializable;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+/**
+ * 
+ * @author carlos martinez
+ *
+ */
 @LocalBean
 @Stateless
 public class EJBUsuario implements Serializable{
