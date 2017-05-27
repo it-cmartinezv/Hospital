e836e606a089313eca499429cd4a709a52828334
Alejandro <alejito.matallana@gmail.com>
2017-05-27 00:40:32 -0500

    solo subo EJB CITA MEDICA Y CONVERTE CITA MEDICA NADA MAS

--- hospital/hospital-negocio/src/main/java/beans/CitaMedicaEJB.java
+++ hospital/hospital-negocio/src/main/java/beans/CitaMedicaEJB.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import entidades.CitaMedica;
 import entidades.Sintoma;
 
 /**
@@ -30,6 +31,15 @@ public class CitaMedicaEJB {
 		Query q = em.createNamedQuery(Sintoma.listarSintomas);
 		List<Sintoma> lista = q.getResultList();
 		return lista;
+	}
+	
+	/**
+	 * MEtodo para buscar una cita por su ID este metodo se utiliza principalmente en el converter
+	 * @param id
+	 * @return
+	 */
+	public CitaMedica buscar(int id){
+		return em.find(CitaMedica.class, id);
 	}
 	
 }
